7e414cdbc574bb43f276bfad40a8ba3f24b66e7f
ThatsFokus <fabi.gander@gmail.com>
2022-11-17 20:46:10 +0100

    addition

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Title = "Battleships";
+
+Console.WriteLine("Press any Key to bombard a random map!");
+Console.ReadKey(true);
+
+char[][] map = Helper.generateField(16, 16);
+
+char[][] mapwithships = Helper.placeShips(map, 3, Helper.Ships.Length2);
+
