815201aee496494240124973e6aefb9ac0cd4c52
ThatsFokus <fabi.gander@gmail.com>
2022-12-02 13:58:13 +0100

    Merge branch 'master' of https://github.com/ThatsFokus/BattleShips

---
+++ MyGame.cs
@@ -0,0 +1,219 @@
+using SkiaSharp;
+using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
+using Silk.NET.Input;
+using Silk.NET.OpenAL;
+class MyGame
+{
+	public int SizeX;
+	public int SizeY;
+	private IWindow window;
+	private SKSurface surface;
+	private SKCanvas canvas;
+	private GL gl;
+	private int cellSize = 50;
+
+	private int distance = 50;
+
+	private SKPaint paint;
+	private List<Key> pressedKeys;
+	private Program battleships;
+	private int state = 0;
+
+	public MyGame(int width, int height, string title){
+		var options = WindowOptions.Default;
+		SizeX = width;
+		SizeY = height;
+		options.Title = title; 
+		options.Size = new Silk.NET.Maths.Vector2D<int>(width, height);
+		options.ShouldSwapAutomatically = false;
+		window = Window.Create(options);
+		window.FramesPerSecond = 60;
+		window.Update += OnUpdate;
+		window.Render += OnRender;
+		window.Load += OnLoad;
+		window.Closing += onClosing;
+	}
+
+	private void OnUpdate(double arg1){
+		switch (state)
+		{
+			case 0:
+				if(pressedKeys.Remove(Key.Space)){
+					state = 1;
+					battleships.GenNewField();
+				}
+				break;
+			case 1:
+			//just wait here
+				break;
+			case 2:
+			if(pressedKeys.Remove(Key.Space))state = 0;
+				break;
+			default:
+				state = 0;
+				break;
+		}
+	}
+
+	private void OnRender(double arg1){
+		switch (state)
+		{
+			case 0:
+				paint.Color = SKColors.Black;
+				paint.TextSize = 60;
+				canvas.DrawText("Battleships", (float)SizeX / 2 - paint.MeasureText("Battleships") / 2, 200, paint);
+				paint.TextSize = 30;
+				canvas.DrawText("Press Space to Play", (float)SizeX / 2 - paint.MeasureText("Press Space to Play") / 2, 500, paint);
+				break;
+			case 1:
+				showMap();
+				break;
+			case 2:
+				paint.Color = SKColors.ForestGreen;
+				paint.TextSize = 50;
+				canvas.DrawText("Winner", (float)SizeX / 2 - paint.MeasureText("Winner") / 2, 100, paint);
+				paint.Color = SKColors.Black;
+				paint.TextSize = 40;
+				canvas.DrawText($"You had to shoot {battleships.ShotsTaken} times", (float)SizeX / 2 - paint.MeasureText($"You had to shoot {battleships.ShotsTaken} times") / 2, 300, paint);
+				paint.TextSize = 30;
+				canvas.DrawText("Press Space to continue", (float)SizeX / 2 - paint.MeasureText("Press Space to continue") / 2, 500, paint);
+				break;
+			default:
+				state = 0;
+				break;
+		}
+		swapBuffers();
+	}
+
+	private void showMap(){
+		SKPaint water = new SKPaint(){
+			Color = SKColors.Aquamarine
+		};
+		SKPaint hit = new SKPaint(){
+			Color = SKColors.SeaGreen
+		};
+		SKPaint miss = new SKPaint(){
+			Color = SKColors.DarkRed
+		};
+		SKPaint bg = new SKPaint(){
+			Color = SKColors.Black
+		};
+		for (int x = 0; x < Program.FieldSize; x++){
+			for (int y = 0; y < Program.FieldSize; y++){
+				switch (battleships.Map[x,y])
+				{
+					case Program.Water:
+						canvas.DrawRect(distance + x * cellSize, distance + y * cellSize, cellSize, cellSize, bg);
+						canvas.DrawRect(distance + 1 + x * cellSize, distance + 1 + y * cellSize, cellSize-2, cellSize-2, water);
+						break;
+					case Program.ShipHit:
+						canvas.DrawRect(distance + x * cellSize, distance + y * cellSize, cellSize, cellSize, bg);
+						canvas.DrawRect(distance + 1 + x * cellSize, distance + 1 + y * cellSize, cellSize-2, cellSize-2, hit);
+						break;
+					case Program.ShipMiss:
+						canvas.DrawRect(distance + x * cellSize, distance + y * cellSize, cellSize, cellSize, bg);
+						canvas.DrawRect(distance + 1 + x * cellSize, distance + 1 + y * cellSize, cellSize-2, cellSize-2, miss);
+						break;
+					//TODO maybe add an indicator that the ship is sunk
+					default:
+						break;
+				}
+			}
+		}
+		bg.TextSize = 30;
+		canvas.DrawText($"Shots Fired: {battleships.ShotsTaken}", distance * 2 + Program.FieldSize * cellSize, distance * 2, bg);
+	}
+
+	private void OnLoad(){
+
+		//create and bind input context
+		var input = window.CreateInput();
+
+		foreach (var keyboard in input.Keyboards){
+			keyboard.KeyDown += OnKeyDown;
+			keyboard.KeyUp += OnKeyUp;
+		}
+
+		foreach (var mouse in input.Mice){
+			mouse.MouseDown += OnMouseDown;
+			mouse.MouseMove += OnMouseMove;
+			mouse.Scroll += OnMouseScroll;
+		}
+
+		//create and configure OpenGL context
+		gl = window.CreateOpenGL();
+		gl.ClearColor(1f, 1f, 1f, 1f);
+
+		//create SkiaSharp context
+		var grinterface = GRGlInterface.CreateOpenGl(name => {
+			if (window.GLContext.TryGetProcAddress(name, out nint fn)) return fn;
+			return (nint)0;
+		});
+
+		var skiabackendcontext = GRContext.CreateGl(grinterface);
+		var format = SKColorType.Rgba8888;
+		var backendrendertarget = new GRBackendRenderTarget(window.Size.X, window.Size.Y, window.Samples ?? 1, window.PreferredStencilBufferBits ?? 16, new GRGlFramebufferInfo(
+			0, format.ToGlSizedFormat()
+		));
+		//var info = new SKImageInfo(window.Size.X, window.Size.Y);
+		//surface = SKSurface.Create(info);
+		surface = SKSurface.Create(skiabackendcontext, backendrendertarget, format);
+		canvas = surface.Canvas;
+		var typeface = SKTypeface.CreateDefault();
+		paint = new SKPaint(new SKFont(typeface));
+
+		//add gameObjects
+		//add ground
+		createObjects();
+	}
+
+	public void Run(){
+		window.Run();
+	}
+
+	private void OnKeyDown(IKeyboard arg1, Key arg2, int arg3){
+		if (arg2 == Key.Escape) window.Close();
+		if (!pressedKeys.Contains(arg2)) pressedKeys.Add(arg2);
+	}
+
+	private void OnKeyUp(IKeyboard arg1, Key arg2, int arg3){
+		pressedKeys.Remove(arg2);
+	}
+
+	private void OnMouseMove(IMouse arg1, System.Numerics.Vector2 arg2){
+
+	}
+
+	private void OnMouseDown(IMouse arg1, MouseButton arg2){
+		if(arg1.Position.X >= 50 && arg1.Position.X <= 50 + Program.FieldSize * cellSize && arg1.Position.Y >= 50 && arg1.Position.Y <= 50 + Program.FieldSize * cellSize && state == 1){
+			float x = (arg1.Position.X - distance) / cellSize;
+			float y = (arg1.Position.Y - distance) / cellSize;
+			x -= x % 1;
+			y -= y % 1;
+			battleships.TakeShot((int)x, (int)y);
+			if(battleships.HasWon) state = 2;
+		}
+	}
+
+	private void OnMouseScroll(IMouse arg1, ScrollWheel arg2){
+
+	}
+	
+
+
+	private void swapBuffers(){
+		canvas.Flush();
+		window.SwapBuffers();
+		canvas.Clear(SKColors.DarkSlateGray); // set the background color here
+	}
+
+	private void createObjects(){
+		//create all variables
+		pressedKeys = new List<Key>();
+		battleships = new Program();
+	}
+
+	void onClosing(){
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,53 @@
 ﻿class Program
 {
-    private readonly char Water = '~';
-	private readonly char ShipHit = '+';
-	private readonly char HiddenShip = '#';
-	private readonly char ShipMiss = 'O';
-	private readonly int[] FieldSize = {10, 10};
+    public const char Water = '~';
+	public const char ShipHit = '+';
+	private const char HiddenShip = '#';
+	public const char ShipMiss = 'O';
+	public const int FieldSize = 10;
+	private char[,] map;
+	private char[,] mapwithships;
+	private int shotsTaken;
+	public int ShotsTaken{
+		get{return shotsTaken;}
+	}
+	public char[,] Map{
+		get{ return map;}
+	}
+
+	private bool hasWon;
+	public bool HasWon{
+		get{return hasWon;}
+	}
+
 
     private static void Main(string[] args){
-        new Program().run();
+        new MyGame(800, 600, "Battleships").Run();
     }
 
+	public Program(){
+		GenNewField();
+	}
+
+	public void GenNewField(){
+		map = generateField();
+		mapwithships = placeShips(generateField(), 1, Ships.Length2);
+		mapwithships = placeShips(mapwithships, 2, Ships.Length3);
+		mapwithships = placeShips(mapwithships, 1, Ships.Length4);
+		mapwithships = placeShips(mapwithships, 1, Ships.Length5);
+		shotsTaken = 0;
+		hasWon = false;
+	}
     public void run(){
-		Console.OutputEncoding = System.Text.Encoding.Unicode;
-		Console.Clear();
-        Console.Title = "Battleships";
-		Console.WriteLine("There will be 1 2TileShip, 2 3TileShip, 1 4TileShip, 1 5TileShip");
-        Console.WriteLine("Press any Key to bombard a random map!");
-        Console.ReadKey(true);
         bool playing = true;
 
         while (playing){
 			
-            char[,] map = generateField();
-
-            char[,] mapwithships = placeShips(generateField(), 1, Ships.Length2);
-			mapwithships = placeShips(mapwithships, 2, Ships.Length3);
-			mapwithships = placeShips(mapwithships, 1, Ships.Length4);
-			mapwithships = placeShips(mapwithships, 1, Ships.Length5);
+            GenNewField();
 			Console.Clear();
 
-            drawMapToScreen(map);
 			bool InRound = true;
-			int shotsTaken = 0;
+			shotsTaken = 0;
 			Console.SetCursorPosition(1,1);
 			while(InRound){
 				Console.CursorVisible = true;
@@ -39,27 +55,25 @@
 				switch (Console.ReadKey(true).Key)
 				{
 					case ConsoleKey.UpArrow:
-						if(cursor.Top <= 1) cursor.Top = FieldSize[1];
+						if(cursor.Top <= 1) cursor.Top = FieldSize;
 						else cursor.Top -= 1;
 						break;
 					case ConsoleKey.DownArrow:
-						if(cursor.Top >= FieldSize[1]) cursor.Top = 1;
+						if(cursor.Top >= FieldSize) cursor.Top = 1;
 						else cursor.Top += 1;
 						break;
 					case ConsoleKey.RightArrow:
-						if(cursor.Left >= FieldSize[0]) cursor.Left = 1;
+						if(cursor.Left >= FieldSize) cursor.Left = 1;
 						else cursor.Left += 1;
 						break;
 					case ConsoleKey.LeftArrow:
-						if(cursor.Left <= 1) cursor.Left = FieldSize[0];
+						if(cursor.Left <= 1) cursor.Left = FieldSize;
 						else cursor.Left -= 1;
 						break;
 					
 					case ConsoleKey.Spacebar:
-						Console.Write(takeShot(cursor.Left -1, cursor.Top - 1, mapwithships, map));
-						Console.ResetColor();
 						shotsTaken++;
-						Console.SetCursorPosition(FieldSize[0] + 5, 0);
+						Console.SetCursorPosition(FieldSize + 5, 0);
 						Console.Write($"Fired cannonballs: {shotsTaken}");
 						break;
 					case ConsoleKey.Escape:
@@ -70,7 +84,7 @@
 						break;
 				}
 				Console.SetCursorPosition(cursor.Left, cursor.Top);
-				if (checkWinState(map, mapwithships))InRound = false;
+				if (checkWinState())InRound = false;
 			}
 			Console.Clear();
 			Console.ForegroundColor = ConsoleColor.Green;
@@ -87,30 +101,29 @@
         }
     }
 
-	private char takeShot(int x, int y, char[, ] mapwithships, char[,] map){
+	public void TakeShot(int x, int y){
+		shotsTaken++;
 		if (mapwithships[x, y] == HiddenShip) {
-			Console.ForegroundColor = ConsoleColor.Green;
 			map[x,y] = ShipHit;
-			return ShipHit;
+			hasWon = checkWinState();
+			return;
 		}
-		Console.ForegroundColor = ConsoleColor.Red;
-		map[x,y] = ShipHit;
-		return ShipMiss;
+		map[x,y] = ShipMiss;
 	}
 
-	private bool checkWinState(char[,] map, char[,] mapwithships){
+	private bool checkWinState(){
 		bool state = true;
-		for (int x = 0; x < FieldSize[0]; x++){
-			for (int y = 0; y < FieldSize[1]; y++){
+		for (int x = 0; x < FieldSize; x++){
+			for (int y = 0; y < FieldSize; y++){
 				if (mapwithships[x, y] == HiddenShip && map[x, y] != ShipHit) state = false;
 			}
 		}
 		return state;
 	}
     private char[,] generateField(){
-		char[,] map = new char[FieldSize[0], FieldSize[1]];
-		for (int i = 0; i < FieldSize[0]; i++){
-			for(int j = 0; j < FieldSize[1]; j++){
+		char[,] map = new char[FieldSize, FieldSize];
+		for (int i = 0; i < FieldSize; i++){
+			for(int j = 0; j < FieldSize; j++){
 			    map[i,j] = Water;
 			}
 		}
@@ -129,8 +142,8 @@
 			case Ships.Length2:
 				if(random.Next(2) == 0){ //vertical
 					
-					int randx = random.Next(FieldSize[0]);
-					int randy = random.Next(FieldSize[1] - 2);
+					int randx = random.Next(FieldSize);
+					int randy = random.Next(FieldSize - 2);
 					bool valid = true;
 					for (int y = randy; y < randy + 2; y++){
 						if(map[randx, y] != Water) valid = false;
@@ -142,8 +155,8 @@
 						placedShips++;
 					}
 				}else{ //horizontal
-					int randx = random.Next(FieldSize[0] - 2);
-					int randy = random.Next(FieldSize[1]);
+					int randx = random.Next(FieldSize - 2);
+					int randy = random.Next(FieldSize);
 					bool valid = true;
 					for (int x = randx; x < randx + 2;x++){
 						if (map[x, randy] != Water) valid = false;
@@ -160,8 +173,8 @@
 				case Ships.Length3:
 					if(random.Next(2) == 0){ //vertical
 					
-					int randx = random.Next(FieldSize[0]);
-					int randy = random.Next(FieldSize[1] - 3);
+					int randx = random.Next(FieldSize);
+					int randy = random.Next(FieldSize - 3);
 					bool valid = true;
 					for (int y = randy; y < randy + 3; y++){
 						if(map[randx, y] != Water) valid = false;
@@ -173,8 +186,8 @@
 						placedShips++;
 					}
 				}else{ //horizontal
-					int randx = random.Next(FieldSize[0] - 3);
-					int randy = random.Next(FieldSize[1]);
+					int randx = random.Next(FieldSize - 3);
+					int randy = random.Next(FieldSize);
 					bool valid = true;
 					for (int x = randx; x < randx + 3;x++){
 						if (map[x, randy] != Water) valid = false;
@@ -191,8 +204,8 @@
 				case Ships.Length4:
 					if(random.Next(2) == 0){ //vertical
 					
-					int randx = random.Next(FieldSize[0]);
-					int randy = random.Next(FieldSize[1] - 4);
+					int randx = random.Next(FieldSize);
+					int randy = random.Next(FieldSize - 4);
 					bool valid = true;
 					for (int y = randy; y < randy + 4; y++){
 						if(map[randx, y] != Water) valid = false;
@@ -204,8 +217,8 @@
 						placedShips++;
 					}
 				}else{ //horizontal
-					int randx = random.Next(FieldSize[0] - 4);
-					int randy = random.Next(FieldSize[1]);
+					int randx = random.Next(FieldSize - 4);
+					int randy = random.Next(FieldSize);
 					bool valid = true;
 					for (int x = randx; x < randx + 4;x++){
 						if (map[x, randy] != Water) valid = false;
@@ -222,8 +235,8 @@
 				case Ships.Length5:
 					if(random.Next(2) == 0){ //vertical
 					
-					int randx = random.Next(FieldSize[0]);
-					int randy = random.Next(FieldSize[1] - 5);
+					int randx = random.Next(FieldSize);
+					int randy = random.Next(FieldSize - 5);
 					bool valid = true;
 					for (int y = randy; y < randy + 5; y++){
 						if(map[randx, y] != Water) valid = false;
@@ -235,8 +248,8 @@
 						placedShips++;
 					}
 				}else{ //horizontal
-					int randx = random.Next(FieldSize[0] - 5);
-					int randy = random.Next(FieldSize[1]);
+					int randx = random.Next(FieldSize - 5);
+					int randy = random.Next(FieldSize);
 					bool valid = true;
 					for (int x = randx; x < randx + 5;x++){
 						if (map[x, randy] != Water) valid = false;
@@ -257,50 +270,6 @@
 		}
 		return map;
 	}
-
-	public void drawMapToScreen(char[,] map){
-		for (int x = 0; x <= FieldSize[0] + 1; x++){
-			for (int y = 0; y <= FieldSize[1] + 1; y++){
-                Console.SetCursorPosition(x,y);
-				if(x == 0 || x == FieldSize[0] + 1){
-					Console.Write('|');
-                    continue;
-				}
-				if(y == 0 || y == FieldSize[1] + 1){
-					Console.Write('-');
-                    continue;
-				}
-				if(map[x-1, y-1] == Water){
-					Console.ForegroundColor = ConsoleColor.Blue;
-				}
-                Console.Write(map[x-1, y-1]);
-				Console.ResetColor();
-			}
-		}
-
-		Console.SetCursorPosition(FieldSize[0] + 5, 0);
-		Console.Write("Fired cannonballs: 0");
-
-		Console.SetCursorPosition(FieldSize[0] + 5, 3);
-		Console.Write("Movement:");
-
-		Console.SetCursorPosition(FieldSize[0] + 9, 4);
-		//Console.Write((char)708);
-		Console.Write((char)8593);
-		Console.SetCursorPosition(FieldSize[0] + 7, 5);
-		Console.Write($"{(char)8592}   {(char)8594}");
-		//Console.Write((char)706);
-		//Console.SetCursorPosition(FieldSize[0] + 10, 5);
-		//Console.Write((char)707);
-		
-		Console.SetCursorPosition(FieldSize[0] + 9, 6);
-		//Console.Write((char)709);
-		Console.Write((char)8595);
-
-		Console.SetCursorPosition(FieldSize[0] + 5, 8);
-		Console.Write("Space to shoot");
-	}
-	
 }
 
 public enum Ships
